94b4a6eb7cd5777172573ea3225e1861b51d0f08
AqueleTaldoGabo <57024740015@unicentro.edu.br>
2025-03-21 13:26:00 -0300

    Add files via upload

---
+++ Ex3 - Java/src/Java.java
@@ -0,0 +1,13 @@
+public class Java {
+    private String nome;
+    public Java(String nome1){
+        nome = nome1;
+    }
+
+    public void setNome(String nome1){
+        nome = nome1;
+    }
+    public String getNome(){
+        return nome;
+    }
+}
